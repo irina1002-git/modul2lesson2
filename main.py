01e85d0281e0bfd251c8904b11952094c9e7943b
irina1002-git <irinazhurenko1980@gmail.com>
2024-11-14 11:01:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+first = 123
+second = 456
+third = 789
+if first == second and first == third and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+first=42
+second=69
+third=42
+if first == second and first == third and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
